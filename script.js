a7f7b10a1ff1aaa6b42d88af812ef517ea2ffc11
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-08-12 16:15:29 +0500

    Styling and HTML completed
    - Added a new book button which displays a dialog form
    
    - Use cards to display books in library

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let myLibrary = [];
+const dialogButton = document.querySelector("#showDialog");
+const myDialog = document.querySelector("#newBookDialog");
+const closeDialogButton = document.querySelector("#formCancel");
+
+closeDialogButton.addEventListener("click", () => {
+    myDialog.close();
+})
+
+dialogButton.addEventListener("click", () => {
+    myDialog.showModal();
+})
 
 function Book() {
     // The constructor
