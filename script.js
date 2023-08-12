34d44cb4af54427a126069090f1d4f8751d6f8a2
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-08-12 17:41:11 +0500

    Added form interaction with javascript

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let myLibrary = [];
+let Book = {};
 const dialogButton = document.querySelector("#showDialog");
 const myDialog = document.querySelector("#newBookDialog");
 const closeDialogButton = document.querySelector("#formCancel");
+const formObj = document.getElementById("dialogForm");
+
+formObj.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formObj.elements[""]
+})
 
 closeDialogButton.addEventListener("click", () => {
     myDialog.close();
@@ -11,10 +18,10 @@ dialogButton.addEventListener("click", () => {
     myDialog.showModal();
 })
 
-function Book() {
+function Book(title, author, page, readStatus) {
     // The constructor
 }
 
-function addBookToLibrary(){
+function addBookToLibrary(bookToAdd){
     // do stuff here
 }
